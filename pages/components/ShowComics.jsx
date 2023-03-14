20ffe8bfff836ffd03db00805bda430d4b505e7b
Sebaex <sebastianparraguez2000@gmail.com>
2023-03-14 02:27:43 -0300

    funny cors error

--- pages/components/ShowComics.jsx
+++ pages/components/ShowComics.jsx
@@ -6,7 +6,7 @@ function ShowComics() {
     const [tlComic, setTLComic] = useState("")
 
     const getLastComic = async () => {
-        const res = await fetch("http://localhost:3000/api/lastComic")
+        const res = await fetch("http://localhost:3000/api/lastComic", {mode: 'no-cors'})
         const comicNumber = await res.json()
         setLastComic(comicNumber.lastComic)
     }
