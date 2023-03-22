da1c7019a49257217eb0dd586429f48665c9dcc9
Sebaex <sebastianparraguez2000@gmail.com>
2023-03-21 22:06:00 -0300

    Date refactoring using Moment-timezone
    Also added a small picture resize and publishing times
    Refactoring made due to negative countdown issues of limited timezone control with vanilla js

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 
 function Countdown({ timezone, targetHour }) {
   const [timeRemaining, setTimeRemaining] = useState(0);
+  const moment = require('moment-timezone');
+  const [jpLaunchTime, setJpLaunchTime] = useState()
+  const [localLaunchTime, setLocalLaunchTime] = useState()
 
   useEffect(() => {
     const timer = setInterval(() => {
-      const now = new Date().toLocaleString("en-US", { timeZone: timezone });
+      const now = moment().tz(timezone);
       const nextTarget = getNextTarget(now);
-      const timeToNextTarget = nextTarget - new Date(now);
+      const timeToNextTarget = nextTarget - now.valueOf();
       setTimeRemaining(timeToNextTarget);
     }, 1000);
 
@@ -15,13 +18,17 @@ function Countdown({ timezone, targetHour }) {
   }, [timezone, targetHour]);
 
   const getNextTarget = (now) => {
-    let targetDate = new Date(now);
-    targetDate.setDate(targetDate.getDate() + ((2 - targetDate.getDay() + 7) % 7));
-    targetDate.setHours(targetHour, 0, 0, 0);
+    let targetDate = moment(now)
+      .add((2 - moment(now).day() + 7) % 7, 'days')
+      .set('hour', targetHour)
+      .set('minute', 0)
+      .set('second', 0)
+    setJpLaunchTime(targetDate.format("HH:mm UTC Z"))
+    setLocalLaunchTime(targetDate.local().format("HH:mm UTC Z"))
     if (targetDate < now) {
-      targetDate.setDate(targetDate.getDate() + 7);
+      targetDate.add(7, 'days');
     }
-    return targetDate.getTime();
+    return targetDate.toDate();
   };
 
   const formatTimeRemaining = (timeRemaining) => {
@@ -30,23 +37,30 @@ function Countdown({ timezone, targetHour }) {
     const hoursCount = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
     const daysCount = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
 
-    const seconds = secondsCount === 1 ? `${secondsCount} second` : `${secondsCount} seconds` 
-    const minutes = minutesCount === 1 ? `${minutesCount} minute` : `${minutesCount} minutes` 
-    const hours = hoursCount === 1 ? `${hoursCount} hour` : `${hoursCount} hours` 
-    const days = daysCount === 1 ? `${daysCount} day` : `${daysCount} days` 
+    const seconds = secondsCount === 1 ? `${secondsCount} second` : `${secondsCount} seconds`
+    const minutes = minutesCount === 1 ? `${minutesCount} minute` : `${minutesCount} minutes`
+    const hours = hoursCount === 1 ? `${hoursCount} hour` : `${hoursCount} hours`
+    const days = daysCount === 1 ? `${daysCount} day` : `${daysCount} days`
 
-    if (daysCount > 0) {
-      return `${days}, ${hours}, ${minutes}, ${seconds}`;
+    if (minutesCount == 0) {
+      return `${seconds}`;
     }
-    else {
+    if (hoursCount == 0) {
+      return `${minutes}, ${seconds}`;
+    }
+    if (daysCount == 0) {
       return `${hours}, ${minutes}, ${seconds}`;
     }
+    return `${days}, ${hours}, ${minutes}, ${seconds}`;
   };
 
   return (
     <div className="mb-5">
       <h1>Time remaining until next comic:</h1>
       <span className="h3">{formatTimeRemaining(timeRemaining)}</span>
+      <h2 className="mt-4">Time launched at:</h2>
+      <div className="col h4">Japan: {jpLaunchTime}</div>
+      <div className="col h4">Your local time: {localLaunchTime}</div>
     </div>
   );
 }
