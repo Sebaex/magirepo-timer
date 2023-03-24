2fe2a86c6328b837849d588de72833d5d23d3541
Sebaex <sebastianparraguez2000@gmail.com>
2023-03-24 03:46:57 -0300

    Changed page title

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Magirepo Timer</title>
+        <title>Magia Report Comic Timer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
       <Component {...pageProps} />
